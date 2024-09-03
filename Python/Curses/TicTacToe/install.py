e507a4c68ca3389931231e72ffa7ea677c3733ed
Miyamoto-creator <85489757+Miyamoto-creator@users.noreply.github.com>
2024-09-04 00:35:00 +0200

    Update install.py

--- Python/Curses/TicTacToe/install.py
+++ Python/Curses/TicTacToe/install.py
@@ -4,4 +4,4 @@ from pip._internal import main as pipmain
 def install_whl(path):
     pip.main(['install', path])
 
-install_whl(r"C:\\Users\\LarsErikHaukås.LT-PF1VY9A4\\OneDrive - Krokeide Videregående Skole\\VG1\\curses-2.2.1+utf8-cp310-cp310-win_amd64.whl")
+install_whl(r"LOL")
